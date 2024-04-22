fab335a1a51d97865ee76608ff5fdfcedad726ae
mcekov <mtsekov@zohomail.eu>
2024-04-22 14:25:07 +0300

    Add styles to tag input

--- src/components/TagInput.js
+++ src/components/TagInput.js
@@ -26,20 +26,20 @@ const TagInput = () => {
   const removeTag = (index) => setTags(prevTags => prevTags.filter((tag, i) => i !== index))
 
   return (
-    <div className='flex border rounded p-2'>
+    <div className='flex border rounded p-2 shadow-sm'>
       {/* <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="tag">
         add tag
       </label> */}
       {tags.map((tag, index) => (
-        <div key={index} className="bg-gray-200 text-gray-800 rounded px-2 py-1 mr-2">
+        <div key={index} className="flex bg-gray-200 text-gray-800 rounded px-2 py-1 mr-2">
           {tag}
 
-          <button onClick={() => removeTag(index)} className="ml-2">&times;</button>
+          <button onClick={() => removeTag(index)} className="ml-2">&#x2715;</button>
         </div>
 
       ))}
 
-      <input id='tag' value={tag} onChange={handleChange} onKeyDown={handleKeyDown} type="text" className="borde-0 outline-0 rounded px-3 py-1" />
+      <input id='tag' value={tag} onChange={handleChange} onKeyDown={handleKeyDown} type="text" className="borde-0 outline-0 rounded px-1 py-1 w-full" />
     </div>
   )
 
